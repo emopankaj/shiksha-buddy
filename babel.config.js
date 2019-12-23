a2f1c177f11da7f5c62f7692726251a2963d38cd
Arjun Lather <arjunslather@gmail.com>
2019-12-23 20:40:19 +0530

    Configure skeleton of coreui

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,14 @@
 module.exports = {
   presets: [
-    '@vue/cli-plugin-babel/preset'
+    ['@vue/app', {
+      polyfills: [
+        'es6.array.find',
+        'es6.array.from',
+        'es6.symbol'
+      ]
+    }],
+    ["@babel/preset-env", {
+      "useBuiltIns": "entry"
+    }]
   ]
-}
+};
